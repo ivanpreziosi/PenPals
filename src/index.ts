b6e7df9bf6e2805335118bb777d7fdfbe6a0f200
Ivan Preziosi <ivan.preziosi@gmail.com>
2019-09-24 00:49:49 +0200

    ..

--- src/index.ts
+++ src/index.ts
@@ -27,20 +27,20 @@ createConnection().then(async connection => {
     Routes.forEach(route => {
         (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
             
-            var isAuth = true;
+            var isAuth = false;
             
             if(!route.isPublic){                
             
                 let userRepository = getRepository(User);
                 var Auth = require('./helpers/PenpalsAuthentication');
                 const authResult = Auth.checkAuth(req,userRepository);
-                authResult.then(result => result !== null && result !== undefined ? res.json(result) : undefined).catch(e => isAuth = false);                
+                authResult.then(result => result !== null && result !== undefined ? this.isAuth = true : isAuth = false).catch(e => isAuth = false);                
             }
             
             if(isAuth){
                 const result = (new (route.controller as any))[route.action](req, res, next);
                 if (result instanceof Promise) {
-                    result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+                    result.then(result => result !== null && result !== undefined ? res.send(result) : undefined).catch(e => res.json(require('./helpers/UnauthorizedResponse')));
 
                 } else if (result !== null && result !== undefined) {
                     res.json(result);
